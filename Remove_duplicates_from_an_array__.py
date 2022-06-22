91b07a8bd521ceb17a5c695fbb65e2b1bed48525
Pavanivelpuri <98639057+Pavanivelpuri@users.noreply.github.com>
2022-06-22 21:28:44 +0530

    m=int(input()) a=list(map(int,input().split())) b=[] count=0 for i in a:     if i not in b and a.count(i)>1:         b.append(i)     if(a.count(i)==1):         b.append(i) print(*b)

---
+++ Remove_duplicates_from_an_array__.py
@@ -0,0 +1,10 @@
+m=int(input())
+a=list(map(int,input().split()))
+b=[]
+count=0
+for i in a:
+    if i not in b and a.count(i)>1:
+        b.append(i)
+    if(a.count(i)==1):
+        b.append(i)
+print(*b)
